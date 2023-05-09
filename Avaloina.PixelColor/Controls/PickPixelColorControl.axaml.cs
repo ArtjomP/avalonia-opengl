2115a162130fe4580ccd37c865a1a5b2c377b8c7
ArtP <artjom@panteam.net>
2023-05-08 23:32:16 -0700

    render OpenGlControl

--- Avaloina.PixelColor/Controls/PickPixelColorControl.axaml.cs
+++ Avaloina.PixelColor/Controls/PickPixelColorControl.axaml.cs
@@ -1,9 +1,28 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Media;
 
 namespace Avaloina.PixelColor.Controls;
 
 public class PickPixelColorControl : TemplatedControl
 {
+    private OpenGlControl? _openGlControl;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        _openGlControl = e.NameScope.Find<OpenGlControl>("PART_OpenGl");
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+        _openGlControl?.Render(context);
+    }
+
+    protected override void OnMeasureInvalidated()
+    {
+        base.OnMeasureInvalidated();
+        _openGlControl?.InvalidateMeasure();
+    }
 }
