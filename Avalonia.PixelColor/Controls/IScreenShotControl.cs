dc573fb1ecd65ba75b10d93e45e29090b31d9706
ArtP <artjom@panteam.net>
2023-05-30 17:31:28 +0300

    update

--- Avalonia.PixelColor/Controls/IScreenShotControl.cs
+++ Avalonia.PixelColor/Controls/IScreenShotControl.cs
@@ -2,7 +2,6 @@
 
 namespace Avalonia.PixelColor.Controls;
 
-
 public interface IScreenShotControl
 {
     void MakeScreenShot(String fullname);
--- Avalonia.PixelColor/TrackPointViewModel.cs
+++ Avalonia.PixelColor/TrackPointViewModel.cs
@@ -4,20 +4,18 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System;
 
-namespace Avalonia.PixelColor
+namespace Avalonia.PixelColor;
+
+public class TrackPointViewModel : ReactiveObject
 {
+    [Reactive]
+    public SolidColorBrush ColorBrush { get; set; } = new SolidColorBrush();
 
-    public class TrackPointViewModel : ReactiveObject
-    {
-        [Reactive]
-        public SolidColorBrush ColorBrush { get; set; } = new SolidColorBrush();
+    [Reactive]
+    public Double RelativeX { get; set; }
 
-        [Reactive]
-        public Double RelativeX { get; set; }
+    [Reactive]
+    public Double RelativeY { get; set; }
 
-        [Reactive]
-        public Double RelativeY { get; set; }
-
-        public TrackPoint Point { get; set; } = new TrackPoint();
-    }
+    public TrackPoint Point { get; set; } = new TrackPoint();
 }
